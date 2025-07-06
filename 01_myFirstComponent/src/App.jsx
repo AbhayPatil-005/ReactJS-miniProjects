c9d9c155e0ea40457fd103d18ef1fffd4da4c76b
AbhayPatil-005 <abhay.official005@gmail.com>
2025-07-06 20:49:01 +0530

    Initial commit: setup mini projects structure

---
+++ 01_myFirstComponent/src/App.jsx
@@ -0,0 +1,12 @@
+import ExpenseItems from "./Components/ExpenseItems";
+
+function App() {
+  return (
+    <div>
+      <h1>Let's get Started</h1>
+      <ExpenseItems></ExpenseItems>
+    </div>
+  );
+}
+
+export default App;
---
+++ 01_myFirstComponent/src/Components/ExpenseItems.jsx
@@ -0,0 +1,12 @@
+// Write your code here:
+function ExpenseItem() {
+  return (
+    <>
+    <p>This is expense item!</p>
+    <p>Food - $5</p>
+    <p>Petrol - $20</p>
+    <p>Movie - $10</p>
+    </>
+  )
+}
+export default ExpenseItem
