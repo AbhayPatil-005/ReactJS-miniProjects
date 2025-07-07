49cc28f6319ff26f983a2394bc971fa2ab429e2a
AbhayPatil-005 <abhay.official005@gmail.com>
2025-07-07 22:19:38 +0530

    Fromatted Date, gave a fresh look that tells year, month name, day date

--- 01_myFirstComponent/src/Components/ExpenseItems.jsx
+++ 01_myFirstComponent/src/Components/ExpenseItems.jsx
@@ -1,19 +1,33 @@
 // Write your code here:
 import './ExpenseItem.css'
 function ExpenseItem(props) {
+  const months = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"]
+    const month = months[props.date.getMonth()];
+    const year = props.date.getFullYear();
+    const date = props.date.getDate();
+    const day = String(date).padStart(2,"0");
 
   return (
     <>
     <div className='expense-item'>
-      <div>{props.date.toISOString()}</div>
+
+      <div>{year}</div>
+      <div>{month}</div>
+      <div>{day}</div>
+
       <div className='expense-item__location'>
         {props.location}
       </div>
+
       <div className='expense-item__description'>
+
         <h2 className='expense-item__h2'>{props.title}</h2>
+
         <div className='expense-item__price'>
           ${props.price}
         </div>
+
       </div>
     </div>
     </>
