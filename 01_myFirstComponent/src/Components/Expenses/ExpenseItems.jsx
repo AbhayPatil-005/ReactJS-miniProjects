33b812cfd2b1a8a677f04cea29f3a2bd451439b0
AbhayPatil-005 <abhay.official005@gmail.com>
2025-07-09 11:23:15 +0530

    Creating proper file Structure

--- 01_myFirstComponent/src/Components/Expenses/ExpenseItems.jsx
+++ 01_myFirstComponent/src/Components/Expenses/ExpenseItems.jsx
@@ -1,7 +1,7 @@
 // Write your code here:
 import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css'
-import Card from './Card'
+import Card from '../UI/Card'
 
 function ExpenseItem(props) {
   return (
