f628902c013d6acd3e212f47e5ec81355eb7cb55
AbhayPatil-005 <abhay.official005@gmail.com>
2025-07-13 22:51:17 +0530

    Add Cancel button to expense form and toggle form visibility

--- 01_myFirstComponent/src/Components/NewExpense/NewExpense.jsx
+++ 01_myFirstComponent/src/Components/NewExpense/NewExpense.jsx
@@ -1,18 +1,33 @@
+import { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+    const [isEditing, setIsEditing] = useState(false);
+
+    const startEditingHandler = () =>{
+        setIsEditing(true);
+    };
+    
+    const stopEditingHandler = () =>{
+        setIsEditing(false);
+    }
+
     const ExpenseSubmitHandle = ( enteredExpenseData )=> {
         const expenseData = {
         ...enteredExpenseData, id:Math.random().toString()
         }
-    props.onExpenseData(expenseData)
+    props.onExpenseData(expenseData);
+    setIsEditing(false);
     }
     
 
     return (
         <div className="new-expense">
-            <ExpenseForm onExpenseSubmit = {ExpenseSubmitHandle}/>
+            {!isEditing &&(
+                <button onClick={startEditingHandler}>Add Expense</button>
+            )}
+            {isEditing && (<ExpenseForm onExpenseSubmit = {ExpenseSubmitHandle} onCancel={stopEditingHandler}/>)}
         </div>
         )
 }
