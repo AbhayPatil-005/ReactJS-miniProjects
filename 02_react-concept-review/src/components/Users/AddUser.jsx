8496fafcdc89e0688e3f3c23aee505542efbe67d
AbhayPatil-005 <abhay.official005@gmail.com>
2025-07-16 22:38:02 +0530

    Collecting users input using useState and resetting the form after form submit

--- 02_react-concept-review/src/components/Users/AddUser.jsx
+++ 02_react-concept-review/src/components/Users/AddUser.jsx
@@ -1,11 +1,24 @@
 // Write your code at relevant places in the code below:
-import Card from "../UI/Card"
-import "./AddUser.css"
-import Button from '../UI/Button'
+import Card from "../UI/Card";
+import "./AddUser.css";
+import Button from '../UI/Button';
+import { useState } from "react";
 
 const AddUser = () => {
+  const [userEnteredName, setUserName] = useState("");
+  const [userEnteredAge, setUserAge] = useState("");
+
+  const setNameHandler =(event)=>{
+    setUserName(event.target.value)
+  }
+  const setAgeHandler =(event)=>{
+    setUserAge(event.target.value)
+  }
   const addUserHandler = (event) => {
     event.preventDefault();
+    console.log(userEnteredName,userEnteredAge);
+    setUserName("");
+    setUserAge("");
   };
 
   return (
@@ -13,10 +26,10 @@ const AddUser = () => {
 
       <form onSubmit={addUserHandler}>
           <label htmlFor="username" >Username</label>
-          <input id="username" type="text" />
+          <input id="username" type="text" value={userEnteredName} onChange={setNameHandler}/>
 
           <label htmlFor="age">Age</label>
-          <input id="age" type="number"  />
+          <input id="age" type="number" onChange={setAgeHandler} value={userEnteredAge}/>
 
           <Button type="submit">Add User</Button>
       </form>
