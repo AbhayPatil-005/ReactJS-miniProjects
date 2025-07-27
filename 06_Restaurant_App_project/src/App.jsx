caa593c1d0d7e3a240b087ff81a00bf2edc0c3ee
AbhayPatil-005 <abhay.official005@gmail.com>
2025-07-28 01:02:42 +0530

    Restaurant App - added Header Component!

---
+++ 06_Restaurant_App_project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, Fragment } from 'react'
+import Header from './Layout/Header'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Fragment>
+    <Header />
+    </Fragment>
+  )
+}
+
+export default App
