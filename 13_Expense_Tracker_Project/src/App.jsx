2559b1e789ca059460802d5a1dc10b4dac4e56df
AbhayPatil-005 <abhay.official005@gmail.com>
2025-09-19 22:16:02 +0530

    Added SignUp page and integrated firebase authentication service for signup

---
+++ 13_Expense_Tracker_Project/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import SignUpPage from './components/auth/SignUpPage';
+import './App.css'
+
+function App() {
+
+  return (
+    <> 
+    <SignUpPage />
+    </>
+  )
+}
+
+export default App
---
+++ 13_Expense_Tracker_Project/src/components/auth/SignUpPage.jsx
@@ -0,0 +1,82 @@
+import './SignUpPage.css';
+import { useState } from 'react';
+
+
+const SignUpPage=()=>{
+    const API_KEY = import.meta.env.VITE_FIREBASE_AUTH_API_KEY;
+    const [email, setEmail] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [error, setError]=useState('');
+
+    const formHandle=async(e)=>{
+        e.preventDefault()
+
+        if(newPassword !== confirmPassword){
+            setError("Passwords do not match")
+            return;
+        }
+        try{
+            const response = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    email: email,
+                    password: newPassword,
+                    returnSecureToken: true,
+                })
+
+            })
+            const data = await response.json()
+            console.log('The user has Successfully signed up: ',data.email)
+        }catch(err){
+            console.error("Failed to post auth details: ",err)
+        }
+        
+        setError("");
+        setEmail("");
+        setConfirmPassword("");
+        setNewPassword("");
+
+    }
+    return(<>
+        <form onSubmit={formHandle} className='signup-form'>
+            <h3>Sign Up</h3>
+            <div className='inputs-div'>
+                <div><label htmlFor="email">Email </label>
+                    <input
+                        type="email"
+                        id='email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required /></div>
+
+                <div><label htmlFor="new-password">New Password </label>
+                    <input
+                        type="password"
+                        id='new-password'
+                        value={newPassword}
+                        onChange={(e) => setNewPassword(e.target.value)}
+                        onKeyDown={() => setError("")}
+                        required /></div>
+
+                <div><label htmlFor="confirm-password">Confirm Password </label>
+                    <input
+                        type="password"
+                        id='confirm-password'
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        onKeyDown={() => setError("")}
+                        required /></div>
+
+                {error && <p style={{color:"red"}}>{error}</p>}
+                <button type='submit'>
+                    Create Account
+                </button>
+                
+            </div>
+        </form>
+    </>)
+}
+
+export default SignUpPage;
