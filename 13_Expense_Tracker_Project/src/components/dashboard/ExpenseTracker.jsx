b9987ede6feeb6ae2ac24ec793e7a708825a6a74
AbhayPatil-005 <abhay.official005@gmail.com>
2025-09-22 22:49:55 +0530

    Added Edit and Delete functionalities - User's can now delete and edit their expenses

--- 13_Expense_Tracker_Project/src/components/dashboard/ExpenseTracker.jsx
+++ 13_Expense_Tracker_Project/src/components/dashboard/ExpenseTracker.jsx
@@ -6,6 +6,8 @@ import NavBar from './NavBar';
 const ExpenseTracker=()=>{
     const BASE_URL = import.meta.env.VITE_FIREBASE_DATABASE_URL;
     const authCtx = useContext(AuthContext);
+
+    const [editingId, setEditingId] = useState(null);
     const [amount, setAmount] = useState("");
     const [description, setDescription] = useState("");
     const [category, setCategory] = useState("Food");
@@ -42,26 +44,45 @@ const ExpenseTracker=()=>{
     const handleSubmit= async(e) => {
         e.preventDefault();
         setLoading(true);
-        const newExpense = {amount, description, category};
+        const expenseData  = {amount, description, category};
         try{
-            const response = await fetch (`${BASE_URL}/expenses/${userId}.json`,{
-                method:"POST",
+            let response;
+            if(editingId){
+             response = await fetch (`${BASE_URL}/expenses/${userId}/${editingId}.json`,{
+                method:"PUT",
                 headers:{"Content-Type":"application/json"},
-                body:JSON.stringify(newExpense),
+                body:JSON.stringify(expenseData ),
             });
+            }else{
+                response = await fetch(`${BASE_URL}/expenses/${userId}.json`,{
+                    method: "POST",
+                    headers: {"Content-Type":"application/json"},
+                    body:JSON.stringify(expenseData),
+                });
+            }
             if (!response.ok){
                 throw new Error("Failed to store expense");
             }
 
             const data = await response.json();
-            const expenseWithId = {id: data.name, ...newExpense};
-            setExpenses((prev)=>[...prev, expenseWithId]);
+
+            if(editingId){
+                setExpenses((prev)=>
+                    prev.map((exp) => 
+                        exp.id === editingId ? { id: editingId, ...expenseData } : exp
+            ))
+            }else{
+                const expenseWithId = {id: data.name, ...expenseData };
+                setExpenses((prev) => [...prev, expenseWithId]);
+            }
             console.log("userId:", userId);
             console.log("token:", authCtx.token);
 
             setAmount("");
             setDescription("");
-            setCategory("Food")
+            setCategory("Food");
+            setEditingId(null);
+
         }catch(err){
             console.error(err);
         }finally{
@@ -71,6 +92,28 @@ const ExpenseTracker=()=>{
     if(!authCtx.token){
         return <p>Please login to view and add expenses</p>;
     }
+
+    const handleDelete =async (id) =>{
+        try{
+            const response = await fetch(`${BASE_URL}/expenses/${userId}/${id}.json`,{
+                method: "DELETE"
+            })
+            if(!response.ok){
+                throw new Error("Failed to delete expense");
+            }
+            console.log("Expense successfully deleted");
+            setExpenses((prev)=> prev.filter((exp)=>exp.id !== id));
+        }catch(err){
+            console.error(err);
+        }
+    }
+
+    const handleEdit =(exp)=>{
+        setEditingId(exp.id);
+        setAmount(exp.amount);
+        setDescription(exp.description);
+        setCategory(exp.category);
+    };
     return (<>
         <NavBar/>
         <div className='expenses-div'>
@@ -107,7 +150,7 @@ const ExpenseTracker=()=>{
                         <option value="Other">Other</option>
                     </select>
                 </div>
-                <button type='submit'>Add Expense</button>
+                <button type='submit'>{editingId ? "Update Expense" : "Add Expense"}</button>
             </form>
 
             <h3 className='expense-heading'>Your Expenses</h3>
@@ -115,7 +158,10 @@ const ExpenseTracker=()=>{
             <ul>
                 {expenses.map((exp) => (
                     <li key={exp.id}>
-                        <span>₹{exp.amount}</span> - {exp.description} - ({exp.category})
+                        <span>₹{exp.amount}</span> - {exp.description} - ({exp.category})<br/>
+                        <button onClick={() => handleEdit(exp)}>Edit</button>
+                        <button onClick={() => handleDelete(exp.id)}>Delete</button>
+
                     </li>
                 ))}
             </ul>
